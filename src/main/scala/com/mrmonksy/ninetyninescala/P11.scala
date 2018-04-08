9186e9e68a656ea29e8e3fdd72663b4f65426fa9
steven <crashoverride025@gmail.com>
2018-04-08 09:57:05 -0500

    Problem 11

--- src/main/scala/com/mrmonksy/ninetyninescala/P11.scala
+++ src/main/scala/com/mrmonksy/ninetyninescala/P11.scala
@@ -3,9 +3,9 @@ package com.mrmonksy.ninetyninescala
 object P11 {
   def encodeModified[T](values: List[T]): List[Any] = {
     val packedResults = P09.pack(values)
-    packedResults.asInstanceOf[List[Any]].map(v=> v match {
-      case a:List[T] if a.size == 1 => a.head
-      case c:List[T] => (c.head, c.size)
-    } )
+    packedResults.asInstanceOf[List[Any]].map(v => v match {
+      case a: List[T] if a.size == 1 => a.head
+      case c: List[T] => (c.head, c.size)
+    })
   }
 }
